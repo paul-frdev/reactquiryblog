20a9a71274d2131b28dbc388a1b4387e318e0df3
paul-frdev <pavelzadyrko@gmail.com>
2022-03-29 23:40:15 +0300

    practice: useQuery isLoading isError error

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,19 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery("post.id", () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) return <h4>Loading...</h4>;
+  if (isError) {
+    return (
+      <>
+        <p>Opps, something went wrong</p>
+        <p>{error}</p>
+      </>
+    );
+  }
 
   return (
     <>
