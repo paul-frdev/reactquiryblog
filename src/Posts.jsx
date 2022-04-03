ecdf7fcaec4014ae371f04ab3e30c52ecd8d7314
paul-frdev <pavelzadyrko@gmail.com>
2022-04-03 23:09:51 +0300

    isfetching

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,15 +15,25 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isLoading, isError, error } = useQuery(
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
+
+  const { data, isLoading, isError, error, isFetching } = useQuery(
     ["posts", currentPage],
     () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData: true,
     }
   );
 
-  if (isLoading) return <h3>Loading...</h3>;
+  if (isFetching) return <h3>Fetching in progress...</h3>;
   if (isError)
     return (
       <>
